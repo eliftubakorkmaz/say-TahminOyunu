31a7f482a8d3894e179ee9cd6f8cd39219c70a08
Elif Tuba Korkmaz <etubasavli@gmail.com>
2023-07-20 17:33:14 +0300

    .

--- sayıTahminOyunu/Program.cs
+++ sayıTahminOyunu/Program.cs
@@ -15,3 +15,39 @@ int tahmin = 0;
 int hak = 5;
 int deneme = 1;
 
+while (sayi != tahmin)
+{
+    Console.WriteLine("Lütfen 1-20 arasında bir sayı yazın:");
+    string tahminEdilenSayi = Console.ReadLine();
+    
+    if (!int.TryParse(tahminEdilenSayi, out tahmin)) 
+    {
+        Console.WriteLine("Lütfen 1-20 arasında bir sayı giriniz.");
+        continue;
+    }
+
+    if (tahmin > 20)
+    {
+        Console.WriteLine("20 dahil olabilir demiştik. 20'den büyük olamaz!");
+        continue;
+    }
+
+    if ( tahmin == sayi)
+    {
+        Console.WriteLine($"Tebrikler!! Tuttuğum sayıyı {hak - 1} hakkın kala ve {deneme}. denemede buldun!!");
+        Console.WriteLine("Daha iyi bir sonuç için yine oyna!");
+        sayi = tahmin;
+        continue;
+    }
+
+    if ( hak == 1)
+    {
+        Console.WriteLine($"Hakkınız bitti. Bilmeniz gereken sayı: {sayi}");
+        break;
+    }
+
+    Console.WriteLine("Tuttuğum sayıyı bilemediniz!");
+    deneme++;
+    hak--;
+
+}
